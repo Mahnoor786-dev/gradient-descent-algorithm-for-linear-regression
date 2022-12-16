6e9c9825e066aae0b4db9b95237a8c7fdc7b84b0
Mahnoor <mahnoorali57se@gmail.com>
2022-12-16 17:18:48 +0500

    updated

--- LinearReg_Builtin.py
+++ LinearReg_Builtin.py
@@ -1,14 +1,22 @@
 from sklearn.linear_model import LinearRegression
 
-# Define the data
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-y = np.array([0, 0, 1, 1])
+# Load the data
+X = [[0, 1], [5, 1], [15, 2], [25, 5], [35, 11], [45, 15], [55, 34], [60, 35]]
+y = [4, 5, 20, 14, 32, 22, 38, 43]
+X_test = [[0, 1], [10, 2], [20, 5], [30, 9], [40, 12]]
+y_test = [4, 7, 15, 22, 25]
 
-# Create the model
+# Create and fit the model
 model = LinearRegression()
-
-# Fit the model to the data
 model.fit(X, y)
 
-# Print the coefficients
-print(model.coef_)
+# Predict the labels for the test data
+predictions = model.predict(X_test)
+
+# Print the coefficients and intercept of the model
+print("Coefficients:", model.coef_)
+print("Intercept:", model.intercept_)
+
+# Calculate the mean squared error
+mse = mean_squared_error(y_test, predictions)
+print("Mean squared error:", mse)
